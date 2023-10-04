fa0e5f941d6356dce024339b7afe8d340f24bcfc
TuqalQasim <qasimovtural572@gmail.com>
2023-10-05 00:24:26 +0400

    add burger menu

---
+++ js/burger.js
@@ -0,0 +1,21 @@
+const burgerBtn = document.getElementById("burgerBtn");
+const burgerMenu = document.querySelector(".burger_menu");
+const closeBtn = document.getElementById("closeBtn");
+function disableScroll() {
+  var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  var scrollLeft = window.pageXOffset || document.documentElement.scrollLeft;
+  window.onscroll = function () {
+    window.scrollTo(scrollLeft, scrollTop);
+  };
+}
+function enableScroll() {
+  window.onscroll = null;
+}
+burgerBtn.addEventListener("click", () => {
+  burgerMenu.classList.add("burger_menu_visible");
+  disableScroll();
+});
+closeBtn.addEventListener("click", () => {
+  burgerMenu.classList.remove("burger_menu_visible");
+  enableScroll();
+});
