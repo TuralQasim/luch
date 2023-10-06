0d4981a41a51596b43dff7d09f611157e42b7410
TuqalQasim <qasimovtural572@gmail.com>
2023-10-06 22:02:30 +0400

    add catalog-card page and write responsive for it

---
+++ js/catalog-card.js
@@ -0,0 +1,4 @@
+document.querySelector(".option_hero").addEventListener("click", () => {
+  document.querySelector(".option_value").classList.toggle("option_value_visible");
+  console.log("salam");
+});
